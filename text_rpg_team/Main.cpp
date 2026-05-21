7711a297e9cdea78a6fb2dff5aa712d3ab396449
mooang lee <natalie12595@gmail.com>
2026-05-21 23:47:45 +0900

    gitfix : 원격 저장소에서 .vs 폴더 추적 제외 및 .gitignore 적용 ( #7 )
    -> .editorconfig추가
    -> .vsconfig추가
    -> rm -r --cached .vs/ 명령어를 통해 원격 저장소에서 .vs 폴더 추적 제거

--- text_rpg_team/Main.cpp
+++ text_rpg_team/Main.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 int main(int argc, char* argv[])
 {
     cout << "Hello World!" << endl;
     // 어디가 이상하죠?
+	//한번 확인해보겠습니다.
     return 0;
 }
