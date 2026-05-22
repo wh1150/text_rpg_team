2b5275c1a1f23e873c3d89db0cfcc4dfb23320ad
azsaii <socczx@naver.com>
2026-05-22 14:31:18 +0900

    feat : 몬스터 종류 클래스 추가, 생성자에서 몬스터 능력치 초기화 (#11)

---
+++ text_rpg_team/HellRabbitMonster.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Monster.h"
+
+class HellRabbitMonster : public Monster {
+private:
+
+	HellRabbitMonster() = delete;
+
+public:
+
+	HellRabbitMonster(int playerLevel);
+
+};
