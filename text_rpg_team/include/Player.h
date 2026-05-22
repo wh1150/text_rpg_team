e148b68499a8e69b49036a6cfa3116caf72d2766
dotori <a01062688790@gmail.com>
2026-05-22 17:00:07 +0900

    feat : setname,getname함수추가(#10)

--- text_rpg_team/include/Player.h
+++ text_rpg_team/include/Player.h
@@ -8,7 +8,7 @@ using namespace std;
 class Player : public Character
 {
 public:
-    Player(const string& name, int level, int exp, int maxExp, int gold);
+    Player(int level, int exp, int maxExp, int gold);
 	int SetHP(int hp);
 	int SetMaxHP(int maxHp);
 	int SetAttackPower(int attackPower);
