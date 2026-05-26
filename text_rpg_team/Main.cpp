c46f609d3b0976d28416b1b00cedd53ecae0e673
mooang lee <natalie12595@gmail.com>
2026-05-26 20:34:24 +0900

    ( #54 )
    몬스터 랜덤 출현
    몬스터 별 아스키아트 추가

--- text_rpg_team/Main.cpp
+++ text_rpg_team/Main.cpp
@@ -3,12 +3,36 @@
 #include <windows.h>
 #include <map>
 #include <string>
+#include <ctime>
 #include "LogManager.h"
 #include "Player.h"
 #include "HellRabbitMonster.h"
+#include "CrazyChihuahuaMonster.h"
+#include "ShadowCatMonster.h"
 #include "Battle.h"
 #include "HealthPotion.h"
 #include "AttackBoost.h"
+
+
+Monster* CreateRandomMonster(int playerLevel)
+{
+	int random = rand() % 3;
+
+	switch (random)
+	{
+	case 0:
+		return new HellRabbitMonster(playerLevel);
+
+	case 1:
+		return new CrazyChihuahuaMonster(playerLevel);
+
+	case 2:
+		return new ShadowCatMonster(playerLevel);
+	}
+
+	return nullptr;
+}
+
 
 using namespace std;
 int main() 
@@ -16,6 +40,7 @@ int main()
 
 	LogManager log;
 	Battle battle;
+	srand((unsigned int)time(nullptr)); // 랜덤값 초기화
 
 	string playerName = log.PrintTitle();
 
@@ -25,13 +50,12 @@ int main()
 
 	//TODO: 초기 아이템 설정 (테스트용) 추후 기본 소지 아이템을 설정할 때 변경이 필요합니다.
 	//TODO: 아이템 클래스를 상속받은 자식 클래스들의 구현이 완료되어야 합니다.
-	//player.AddItem(make_unique<HealthPotion>());
-	//player.AddItem(make_unique<HealthPotion>());
-	//player.AddItem(make_unique<AttackBoost>());
+	player.AddItem(make_unique<HealthPotion>());
+	player.AddItem(make_unique<HealthPotion>());
+	player.AddItem(make_unique<AttackBoost>());
 
-	// TODO: 임시로 처음 등장 시켜놓은 몬스터입니다. 추후 변경이 필요합니다.
-	HellRabbitMonster monster(player.GetLevel());
-	// std::map<std::string, int> inventory = { {"체력 포션", 3}, {"힘 포션", 3} };
+
+	Monster* monster = CreateRandomMonster(player.GetLevel());
 	/*
 	* 로그 출력 가이드:
 	* log.Print(Message) : 단순 메시지 출력. 가장 아래쪽 로그창에 출력됩니다.
@@ -41,13 +65,13 @@ int main()
 
 	// 전투 시작
 	// log.PrintStatus(player);
-	log.PrintStartBattle(player, monster);
+	log.PrintStartBattle(player, *monster);
 	log.PrintMenu("1. 공격  2. 상점  3. 도망 ");
 	log.PrintInventory(player.GetInventory());
 
 
 	// 전투 루프 (선택지 입력 대기)
-	while (monster.GetHp() > 0)
+	while (monster->GetHp() > 0)
 	{
 		log.PrintChoice("어떻게 할까? ");
 
@@ -56,7 +80,7 @@ int main()
 
 		if (input == '1')
 		{
-			battle.StartBattle(player, monster);
+			battle.StartBattle(player, *monster);
 		}
 		else if (input == '2')
 		{
@@ -72,5 +96,6 @@ int main()
 			log.Print("잘못된 입력입니다. 다시 선택해주세요.");
 		}
 	}
+	delete monster;
     return 0;
 }
